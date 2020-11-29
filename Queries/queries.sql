9e396246817423fb961dff2be39e34684d0cf8a3
Sami <samiulsaqi1@gmail.com>
2020-11-29 10:17:09 -0500

    added sql to filter out results

---
+++ Queries/queries.sql
@@ -0,0 +1,17 @@
+--retirement eligibality
+select first_name, last_name from employees where (birth_date between '1952-01-01' and '1955-12-31') and (hire_date between '1985-01-01' and '1988-12-31')
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--retirement info writing to table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info
--- Schema.sql
+++ Schema.sql
@@ -47,9 +47,9 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, title, to_date)
 	);
